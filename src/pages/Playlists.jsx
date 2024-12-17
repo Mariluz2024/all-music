7e7956de68513ea96bdbb2025798bd0b41d6b4eb
Mariluz2024 <mluzco29@gmail.com>
2024-12-17 22:06:09 +0100

    Instalación de Axios

--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Playlists = () => {
   return (
     <div className="container text-center mt-5">
-      <h2>Playlists</h2>
       <p>Esta es la página de listas de reproducción.</p>
     </div>
   );
