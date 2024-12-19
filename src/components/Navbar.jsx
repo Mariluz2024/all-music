f319700daeab84226de82d4722c0e310ebe557fd
Mariluz2024 <mluzco29@gmail.com>
2024-12-20 00:01:19 +0100

    Logo de la aplicación

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{background: '#AF4D98'}}>
-      <div className="container-fluid">
-        <Link className="navbar-brand fw-bold" to="/">
+    <nav
+      className="navbar navbar-expand-lg navbar-dark fixed-top"
+      style={{ background: "#AF4D98" }}
+    >
+      <div className="container-fluid d-flex align-items-center">
+        <Link className="navbar-brand fw-bold d-flex align-items-center" to="/">
+          {/* Logo */}
+          <img
+            src="/logo.png" // Replace with your actual image path
+            alt="Logo"
+            style={{
+              width: "30px",
+              height: "30px",
+              objectFit: "contain",
+              marginRight: "10px",
+            }}
+          />
           All Music
         </Link>
         <div className="collapse navbar-collapse">
