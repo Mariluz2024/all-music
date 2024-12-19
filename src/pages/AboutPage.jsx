8c810e055d20d720e3c06377c4b42251b2c6b429
Mariluz2024 <mluzco29@gmail.com>
2024-12-20 00:02:31 +0100

    Remoción de logo

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,11 +6,7 @@ const About = () => {
       <div className="row">
         {/* Image or avatar (use a placeholder or a custom image) */}
         <div className="col-md-4 text-center">
-          <img
-            src="https://private-avatars.githubusercontent.com/u/188079659?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MzQ2NDU2MDAsIm5iZiI6MTczNDY0NDQwMCwicGF0aCI6Ii91LzE4ODA3OTY1OSJ9.bUA75q6KMrbwoGJIp8Nvos9glv4Xysre6f5DStitZQY&v=4"
-            alt="Mariluz Avatar"
-            className="img-fluid shadow"
-          />
+          
         </div>
         {/* Developer's information */}
         <div className="col-md-8 d-flex flex-column justify-content-center">
